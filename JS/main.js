fafe9fb2e08e40e42120288475fd350425eab69b
AdKiNaTOr <arslannormuminovxbox@gmail.com>
2026-03-06 19:48:46 +0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,19 @@
-window.onload = function() {
-    const url = new URL(window.location.href);
-    const fileName = url.pathname.split("/").pop();
-    if (fileName == "index.html") {
-        let download = document.getElementById("download")
-        if (download) {
-            download.addEventListener("click", function(){
-                alert("ЕЩЕ РАНО")
-            })
-        }
-    }
-    if (fileName == "download.html" ) {
-        let download = document.getElementById("downloadOnSite")
-        let AntonChigur = document.getElementById("AntonChigur")
+
+const url = new URL(window.location.href);
+const fileName = url.pathname.split("/").pop();
+if (fileName == "index.html") {
+    let download = document.getElementById("download")
+    if (download) {
         download.addEventListener("click", function(){
-            AntonChigur.classList.add("MakeVisible")
+            alert("ЕЩЕ РАНО")
         })
     }
- 
 }
+if (fileName == "download.html" ) {
+    let download = document.getElementById("downloadOnSite")
+    let AntonChigur = document.getElementById("AntonChigur")
+    download.addEventListener("click", function(){
+        AntonChigur.classList.add("MakeVisible")
+    })
+}
+
